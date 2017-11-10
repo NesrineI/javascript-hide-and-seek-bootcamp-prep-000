9d2cd20bdc104c216e2ad5567b58930db565639c
Nesrine <nesrinakon@gmail.com>
2017-11-10 20:40:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
+function increaseRankBy(n){
+  return
+}
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('.ranked-list li')
+  for(let i = 0 ; i<lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+n;
+  }
+}
