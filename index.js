19ffdd357537a83d18c8430f30ad4a6ff5e8ae40
Nesrine <nesrinakon@gmail.com>
2017-11-10 20:39:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function increaseRankBy(n){
 function increaseRankBy(n) {
   let lis = document.querySelectorAll('.ranked-list li')
   for(let i = 0 ; i<lis.length; i++){
-    lis[i].innerHTML += n;
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+n;
   }
 }
