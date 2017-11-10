4d6c5e5ed974bfecea8b317a9cf8fb96224cd665
Nesrine <nesrinakon@gmail.com>
2017-11-10 20:38:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function nestedTarget() {
 function deepestChild(){
   return document.querySelector('#grand-node div div div div')
 }
+function increaseRankBy(n){
+  return
+}
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('.ranked-list li')
+  for(let i = 0 ; i<lis.length; i++){
+    lis[i].innerHTML += n; 
+  }
+}
